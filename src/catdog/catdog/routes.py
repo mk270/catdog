b33b84aceeafed7cb6bf65167284598c3f2b8d61
Martin Keegan <martin@no.ucant.org>
2012-08-23 23:36:30 +0100

    clean up routing

--- src/catdog/catdog/routes.py
+++ src/catdog/catdog/routes.py
@@ -8,23 +8,26 @@ template = file('static/index.html').read()
 def render(data):
     return pystache.render(template, data)
 
-def get_campaigns():
+def campaigns():
     for cmp in Campaign.query.all():
         yield cmp.to_dict()
+
+def recipients(camp_id):
+    for rcpt in Recipient.query.filter(Recipient.campaign_id==camp_id):
+        yield rcpt.person.to_dict()
 
 @app.route('/campaign/new')
 def new_campaign():
     return "placeholder for create new campaign"
 
 @app.route('/campaign/<camp_id>')
-def recipients(camp_id):
+def campaign_detail(camp_id):
     return render({
             "recipient_list": True,
-            "campaign": get_campaigns(),
-            "recipient":
-             [ rcpt.person.to_dict() for rcpt in Recipient.query.filter(Recipient.campaign_id==camp_id) ]
+            "campaign": campaigns(),
+            "recipient": recipients(camp_id)
             })
 
 @app.route('/')
 def hello_world():
-    return render({ "home_page": True, "campaign": get_campaigns() })
+    return render({ "home_page": True, "campaign": campaigns() })
