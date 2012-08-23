597e5c76223a025084e6a59839380e7a0ce8d312
Martin Keegan <martin@no.ucant.org>
2012-08-24 00:04:05 +0100

    erect more scaffolding for campaign

--- src/catdog/catdog/routes.py
+++ src/catdog/catdog/routes.py
@@ -3,9 +3,9 @@ import pystache
 from catdog import app
 from models import *
 
-template = file('static/index.html').read()
 
 def render(data):
+    template = file('static/index.html').read()
     return pystache.render(template, data)
 
 def campaigns():
@@ -20,12 +20,21 @@ def recipients(camp_id):
 def new_campaign():
     return "placeholder for create new campaign"
 
-@app.route('/campaign/<camp_id>')
-def campaign_detail(camp_id):
+@app.route('/recipients/<camp_id>')
+def recipient_list(camp_id):
     return render({
             "recipient_list": True,
             "campaign": campaigns(),
             "recipient": recipients(camp_id)
+            })
+
+@app.route('/campaign/<camp_id>')
+def campaign_detail(camp_id):
+    return render({
+            "campaign_detail": True,
+            "name": Campaign.query.filter(Campaign.campaign_id==camp_id).first().camp_name,
+            "camp_id": camp_id,
+            "campaign": campaigns()
             })
 
 @app.route('/')
