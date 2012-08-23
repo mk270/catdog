00481c017a9a1fc916eccf8e30f14d8e6966f4b7
Martin Keegan <martin@no.ucant.org>
2012-08-23 23:28:39 +0100

    factor out templating

--- src/catdog/catdog/routes.py
+++ src/catdog/catdog/routes.py
@@ -3,26 +3,28 @@ import pystache
 from catdog import app
 from models import *
 
+template = file('static/index.html').read()
+
+def render(data):
+    return pystache.render(template, data)
+
 def get_campaigns():
     for cmp in Campaign.query.all():
         yield cmp.to_dict()
 
-@app.route('/recipients/<camp_id>')
+@app.route('/campaign/new')
+def new_campaign():
+    return "placeholder for create new campaign"
+
+@app.route('/campaign/<camp_id>')
 def recipients(camp_id):
-    data = file('static/index.html').read()
-    s = pystache.render(data, {
+    return render({
+            "recipient_list": True,
+            "campaign": get_campaigns(),
             "recipient":
              [ rcpt.person.to_dict() for rcpt in Recipient.query.filter(Recipient.campaign_id==camp_id) ]
             })
-    return s
-
-@app.route('/campaign/<camp_id>')
-def campaign(camp_id):
-    data = file('static/index.html').read()
-    return "campid: %s" % camp_id
 
 @app.route('/')
 def hello_world():
-    data = file('static/index.html').read()
-    s = pystache.render(data, { "campaign": get_campaigns() })
-    return s
+    return render({ "home_page": True, "campaign": get_campaigns() })
